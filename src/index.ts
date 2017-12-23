001bc0a1d5b24ba63d624799c3be1d7d756d3bba
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-12-23 19:18:01 +0100

    Updated arguments for graphql-static-binding v0.2.0

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ export const command = 'bindings [schema]'
 export const describe = 'Generate static bindings for the schema and output to a single code file'
 
 import { generateCode} from 'graphql-static-binding'
-import { generator as tsgenerator } from 'graphql-static-binding/dist/generators/typescript'
-import { generator as jsgenerator } from 'graphql-static-binding/dist/generators/javascript'
 import * as fs from 'fs';
 import * as path from 'path';
 import chalk from 'chalk';
@@ -21,15 +19,10 @@ export const builder = {
         requiresArg: true,
         type: 'string'
     },
-    typescript: {
-        alias: 'ts',
-        describe: 'Generate a Typescript code file',
-        type: 'boolean'
-    },
-    javascript: {
-        alias: 'js',
-        describe: 'Generate a Javascript code file',
-        type: 'boolean'
+    generator: {
+        alias: 'g',
+        describe: 'Generator used to generate binding files',
+        type: 'string'
     },
     force: {
         alias: 'f',
@@ -42,29 +35,16 @@ export const handler = async (context, argv) => {
     const schemaFile: string = argv.schema || context.getProjectConfig().schemaPath
     const flag = argv.force ? 'w' : 'wx'
 
+    context.spinner.start(`Loading schema ${chalk.green(path.relative('.', schemaFile))}...`)
     const schema = fs.readFileSync(schemaFile, 'utf-8')
+    context.spinner.succeed(`Loaded schema ${chalk.green(path.relative('.', schemaFile))}`)
 
-    let finalSchema: string
-    if (argv.typescript) {
-        context.spinner.start(`Generating Typescript bindings for schema ${chalk.green(path.relative('.', schemaFile))}...`)
-        finalSchema = generateCode(schema, tsgenerator)
-        context.spinner.succeed()
+    context.spinner.start(`Generating bindings using generator ${chalk.green(argv.generator)}...`)
+    const finalSchema: string = generateCode(schema, argv.generator)
+    context.spinner.succeed(`Generated bindings using generator ${chalk.green(argv.generator)}`)
 
-        const filename = argv.output.endsWith('.ts') ? argv.output : `${argv.output}.ts`
-        context.spinner.start(`Writing code to file ${chalk.green(path.relative('.', filename))}...`)
-        fs.writeFileSync(filename, finalSchema, { flag })
-        context.spinner.succeed()
-    }
-
-    if (argv.javascript) {
-        context.spinner.start(`Generating Javascript bindings for schema ${chalk.green(path.relative('.', schemaFile))}...`)
-        finalSchema = generateCode(schema, jsgenerator)
-        context.spinner.succeed()
-
-        const filename = argv.output.endsWith('.js') ? argv.output : `${argv.output}.js`
-        context.spinner.start(`Writing code to file ${chalk.green(path.relative('.', filename))}...`)
-        fs.writeFileSync(filename, finalSchema, { flag })
-        context.spinner.succeed()
-    }
+    context.spinner.start(`Writing code to file ${chalk.green(path.relative('.', argv.output))}...`)
+    fs.writeFileSync(argv.output, finalSchema, { flag })
+    context.spinner.succeed(`Written code to file ${chalk.green(path.relative('.', argv.output))}`)
 
 }
